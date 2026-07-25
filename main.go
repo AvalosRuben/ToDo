156d0c52b53d95bc1a2f0d1f81f705313a0ea9f4
AvalosRuben <rubenavalosmtz@gmail.com>
2026-07-25 10:37:20 -0600

    feat(GinGonic): Initialized the gin router

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello :p")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context){
+		c.JSON(200, gin.H{"message":"Welcome to the ToDo API!"})
+	})
+
+	r.Run()
 }
