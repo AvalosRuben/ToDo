bb6d3ae86bca2c282dadb9e7bcbc14bdbe38a531
AvalosRuben <rubenavalosmtz@gmail.com>
2026-07-26 20:38:34 -0600

    feat(Task): Added props for the Task component

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -6,9 +6,9 @@ function App() {
       <div className="flex flex-col gap-10 justify-center h-screen py-20 w-1/2 items-center">
         <h1 className="text-5xl font-bold text-slate-600">To Do List</h1>
         <div className="h-full w-full flex flex-col items-center gap-5">
-          <Task />
-          <Task />
-          <Task />
+          <Task taskName="Task name 1" description="description 1" />
+          <Task taskName="Task name 2" description="description 2" />
+          <Task taskName="Task name 3" description="description 3" />
         </div>
       </div>
     </div>
--- Front/src/Task.tsx
+++ Front/src/Task.tsx
@@ -1,8 +1,13 @@
-function Task() {
+interface TaskProps {
+  taskName: string;
+  description: string;
+}
+
+function Task({ taskName, description }: TaskProps) {
   return (
     <div className="w-full h-30 rounded-2xl border border-slate-500 bg-slate-400 p-4 flex flex-col gap-4 justify-center text-lg text-slate-200 font-bold">
-      <p>Task: Task Name</p>
-      <p>Description: This is the detailed description of the task!</p>
+      <p>Task: {taskName}</p>
+      <p>Description: {description}</p>
     </div>
   );
 }
