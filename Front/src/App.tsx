1cd998276f64d9a649a3866b0ffd31965672b85f
AvalosRuben <rubenavalosmtz@gmail.com>
2026-07-26 20:45:59 -0600

    feat(Task): Added a thin scrollbar for aesthetics

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="flex items-center justify-center w-full min-h-screen bg-slate-300">
       <div className="flex flex-col gap-10 justify-center h-screen py-20 w-1/2 items-center">
         <h1 className="text-5xl font-bold text-slate-600">To Do List</h1>
-        <div className="h-full w-full flex flex-col items-center gap-5 max-h-300 min-h-150 overflow-auto">
+        <div className="h-full w-full flex flex-col items-center gap-5 max-h-300 min-h-150 overflow-auto scrollbar-thin">
           <Task taskName="Task name 1" description="description 1" />
           <Task taskName="Task name 2" description="description 2" />
           <Task taskName="Task name 3" description="description 3" />
