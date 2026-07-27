65a781326f22791c37d7065bf1519afd8daf94fe
AvalosRuben <rubenavalosmtz@gmail.com>
2026-07-27 10:52:22 -0600

    feat(ToggleDone): Added the changes on the text when a task is done and added a key and description to the component

--- Front/src/Task.tsx
+++ Front/src/Task.tsx
@@ -1,10 +1,12 @@
 import type { Tasks } from "./Tasks";
 
-function Task({ name, description }: Tasks) {
+function Task({ name, description, done }: Tasks) {
   return (
     <div className="w-full h-30 rounded-2xl border border-slate-500 bg-slate-400 p-4 flex flex-col gap-4 justify-center text-lg text-slate-200 font-bold">
-      <p>Task: {name}</p>
-      <p>Description: {description}</p>
+      <p className={`${done ? "line-through" : ""}`}>Task: {name}</p>
+      <p className={`${done ? "line-through" : ""}`}>
+        Description: {description}
+      </p>
     </div>
   );
 }
