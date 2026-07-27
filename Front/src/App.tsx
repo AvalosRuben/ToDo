a18c2e2587f484a0a1a8db453a42b409997a7d18
AvalosRuben <rubenavalosmtz@gmail.com>
2026-07-26 20:27:08 -0600

    feat(Layout): Basic layout without tasks

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div className="flex items-center justify-center w-full min-h-screen">
-      <p>Hello World!</p>
+    <div className="flex items-center justify-center w-full min-h-screen bg-slate-300">
+      <div className="flex flex-col gap-10 justify-center h-screen py-20">
+        <h1 className="text-5xl font-bold text-slate-600">To Do List</h1>
+        <div className="h-full w-full flex flex-col items-center text-slate-500">
+          Here will go the Tasks!
+        </div>
+      </div>
     </div>
   );
 }
