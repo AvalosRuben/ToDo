5b2cbd5ab2b00fd31c8247d7452a71bf2edc1d9a
AvalosRuben <rubenavalosmtz@gmail.com>
2026-07-26 21:47:23 -0600

    feat(FetchTasks): Created the fecth function that gets all the tasks

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from "react";
 import Task from "./Task";
+import type { Tasks } from "./Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState<Tasks[]>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("http://localhost:8080/get-tasks");
+
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: Failed to fetch tasks`);
+        }
+
+        const data: Tasks[] = await response.json();
+        setTasks(data);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : "An error occurred");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
   return (
     <div className="flex items-center justify-center w-full min-h-screen bg-slate-300">
       <div className="flex flex-col gap-10 justify-center h-screen py-20 w-1/2 items-center">
         <h1 className="text-5xl font-bold text-slate-600">To Do List</h1>
-        <div className="h-full w-full flex flex-col items-center gap-5 max-h-300 min-h-150 overflow-auto scrollbar-thin">
-          <Task taskName="Task name 1" description="description 1" />
-          <Task taskName="Task name 2" description="description 2" />
-          <Task taskName="Task name 3" description="description 3" />
-          <Task taskName="Task name 4" description="description 4" />
-          <Task taskName="Task name 5" description="description 5" />
-          <Task taskName="Task name 6" description="description 6" />
-          <Task taskName="Task name 7" description="description 7" />
-          <Task taskName="Task name 8" description="description 8" />
-        </div>
+        {tasks?.length === 0 ? (
+          <p>No task found :c</p>
+        ) : (
+          <div className="h-full w-full flex flex-col items-center gap-5 max-h-300 min-h-150 overflow-auto scrollbar-thin">
+            {tasks?.map((t) => (
+              <Task name={t.name} description={t.description} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
